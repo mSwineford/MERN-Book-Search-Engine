7b77b06a2e97ab7af3d1e1b30603319deb56b10e
Matthew Swineford <m.swineford@yahoo.com>
2023-01-21 20:24:47 -0500

    added schemas and its files

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const { resolvers, typeDefs } = require("./schemas");
 const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth.js");
 const mongoose = require("mongoose");
