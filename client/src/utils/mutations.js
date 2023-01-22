c847fd7a5caa85469cf32e9da1dad6f75cec0f6c
Matthew Swineford <m.swineford@yahoo.com>
2023-01-22 15:55:53 -0500

    set up mutations.js and its  mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,60 @@
+import gql from "graphql-tag";
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token 
+            use {
+                _id
+                username
+            }
+        }
+    }
+`;
+export const SAVE_BOOK = gql`
+    mutation saveBook($book: bookInput!) {
+        saveBook(input: $input) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removedBook(bookId: $bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
