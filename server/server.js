038019793079e97dd6306e9927c7500cb8ad5e5e
Matthew Swineford <m.swineford@yahoo.com>
2023-01-21 20:11:13 -0500

    added the requirements for apollo, middleware, and mongoose

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const { ApolloServer } = require("apollo-server-express");
+const { authMiddleware } = require("./utils/auth.js");
+const mongoose = require("mongoose");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
