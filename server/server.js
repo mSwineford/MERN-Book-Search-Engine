55452d3657669d3a205d72afbba3d6eaf23aaebc
Matthew Swineford <m.swineford@yahoo.com>
2023-01-25 16:52:18 -0500

    Worked on the README

--- server/server.js
+++ server/server.js
@@ -36,8 +36,8 @@ mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost:27017/booksDB", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    //useCreateIndex: true,
-    //useFindAndModify: false
+    useCreateIndex: true,
+    useFindAndModify: false
   }
 );
 
